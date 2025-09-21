0597ab673701a5888b14b2860f9db5497b2cda9c
Jan Noha <nohajc@gmail.com>
2025-09-21 16:03:55 +0200

    pack kernel modules as squashfs

--- pack-modules.sh
+++ pack-modules.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-tar cvzf modules.tar.gz -C linux-6.12.34 modules.builtin fs/zfs/{zfs,spl}.ko
+cd linux-6.12.34
+mksquashfs modules.builtin fs/zfs/{zfs,spl}.ko ../modules.squashfs -no-strip

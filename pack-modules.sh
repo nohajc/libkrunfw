1770c1b9d6d7b1b675a2d15155d910bc7d51c6fb
Jan Noha <nohajc@gmail.com>
2026-01-09 21:12:43 +0100

    pack-modules.sh updated

--- pack-modules.sh
+++ pack-modules.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+rm modules.squashfs
 cd linux-6.12.34
-mksquashfs modules.builtin fs/zfs/{zfs,spl}.ko ../modules.squashfs -no-strip
+mksquashfs modules.builtin fs/zfs/{zfs,spl}.ko ../modules.squashfs -no-strip -comp xz

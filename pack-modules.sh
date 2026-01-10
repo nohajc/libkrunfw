0a32adbe1d41b9f7eb08702b8e0488d2a38f36e9
Jan Noha <nohajc@gmail.com>
2026-01-10 13:24:12 +0100

    pack-modules.sh: more improvements

--- pack-modules.sh
+++ pack-modules.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 VER=6.12.62
 
-rm modules.squashfs
+rm modules.squashfs || true
 cd linux-$VER
 mksquashfs modules.builtin fs/zfs/{zfs,spl}.ko ../modules.squashfs -no-strip -comp xz
